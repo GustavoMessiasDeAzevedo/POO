cf65a8ce936da9e572c5df239f629236418a51e0
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2025-09-19 20:21:53 -0300

    Herança e polimorfismo

--- 09_Herança/Pessoa.cs
+++ 09_Herança/Pessoa.cs
@@ -8,6 +8,7 @@ namespace _09_Herança
 {
     public abstract class Pessoa
     {
+        public int Id { get; set; }
         public string Nome;
         public string Telefone;
 
@@ -16,6 +17,25 @@ namespace _09_Herança
         public void Imprimir()
         {
             Console.WriteLine($"LISTA DE PESSOAS \n\nNome: {this.Nome} Documento: {this.GetDocumento()} Telefone: {this.Telefone}");
+        }
+
+        public override string ToString()
+        {
+            return $"\n<ToString>Nome: {this.Nome} Documento: {this.GetDocumento()} Telefone: {this.Telefone}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj == null /*|| this.GetType() != obj.GetType()*/)
+                return false;
+            //Cast => Conversão de objeto
+            //( (Pessoa)obj ).Id => Converte obj que é do tipo object para o tipo Pessoa    
+            return this.Id == ((Pessoa)obj).Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
         }
     }
 }
--- 09_Herança/Program.cs
+++ 09_Herança/Program.cs
@@ -10,17 +10,35 @@ namespace _09_Herança
     {
         static void Main(string[] args)
         {
+
+            Pessoa[] pessoas = new Pessoa[2];
+
             PessoaFisica pessoaFisica = new PessoaFisica();
+            pessoas[0] = pessoaFisica;
+            pessoaFisica.Id = 1;
             pessoaFisica.Nome = "Gustavo";
             pessoaFisica.CPF = "123.456.789-00";
             pessoaFisica.Telefone = "(11) 91234-5678";
             pessoaFisica.Imprimir();
 
             PessoaJuridica pessoaJuridica = new PessoaJuridica();
+            pessoas[1] = pessoaJuridica;
+            pessoaJuridica.Id = 1;
             pessoaJuridica.Nome = "Giovana";
             pessoaJuridica.CNPJ = "12.345.678/0001-00";
             pessoaJuridica.Telefone = "(11) 98765-4321";
             pessoaJuridica.Imprimir();
+
+            Console.WriteLine(pessoaJuridica);
+            Console.WriteLine(pessoaFisica);
+
+            Console.WriteLine($"É a mesma pessoa:  {(pessoaFisica.Equals(pessoaJuridica) ? "SIM" : "NÃO")}");
+
+
+            foreach (Pessoa pessoa in pessoas)
+            {
+                Console.WriteLine(pessoa);
+            }
         }
     }
 }
