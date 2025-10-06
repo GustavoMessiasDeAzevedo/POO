e7024e382d17716c7a46bb528e7922b98b605bee
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2025-10-06 20:43:19 -0300

    exception

---
+++ 19_TratamentoExcecao/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_TratamentoExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            while (true)
+            {
+                Console.Write("Digite um numero inteiro: ");
+                try
+                {
+                    numero = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Numero invalido, informe apenas valores inteiros!\n Pressione qualquer tecla pra continuar");
+                    Console.ReadKey()
+                finally
+                {
+                    
+                    Console.Clear();
+                }
+
+            }
+
+            if (numero % 2 == 0)
+                Console.WriteLine("Número par!");
+            else
+                Console.WriteLine("Número ímpar!");
+        }
+    }
+}
+ 
